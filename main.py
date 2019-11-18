2177b2f75777e262506307f3e7b8100300253a80
deepak mishra <dkm.5853@gmail.com>
2019-11-18 12:43:46 +0530

    [Deepak Mishra] Add all programs for unit testing

---
+++ main.py
@@ -0,0 +1,6 @@
+import unittest
+class SimpleTest(unittest.TestCase):
+    def test(self):
+        self.assertTrue(True)
+if __name__=="__main__":
+    unittest.main()
--- readme.md
+++
@@ -1,2 +0,0 @@
-1: all programs done in other branch . just ckeck branch
-2: function and algorithm , oops and chatApp is there
