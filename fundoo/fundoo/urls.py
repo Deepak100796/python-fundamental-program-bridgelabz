d4a560cfbc0bb983168d866f309152ecfa2ca2cb
deepak mishra <dkm.5853@gmail.com>
2019-12-07 18:57:40 +0530

    [Deepak Mishra]Modified fundoo.seting.py and fundoo.urls.py

--- fundoo/fundoo/urls.py
+++ fundoo/fundoo/urls.py
@@ -16,9 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from .views import serverinfo
+from django.conf.urls import url, include
+# from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/',include('authapp.urls')),
-    path('checkserver/', serverinfo , name ='serverinfo')
+    path('checkserver/', serverinfo , name ='serverinfo'),
+    # path('',include('authapp.urls'))
+    url(r'^file/', include('authapp.urls'))
     
 ]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
