c50cc3e4838fe48eca2e7e0769fcf4dcaab7b4fc
deepak mishra <dkm.5853@gmail.com>
2019-12-14 14:41:37 +0530

    [Deepak Mishra]Register model to admin.py

--- fundoo/note/admin.py
+++ fundoo/note/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from note.documents import NotesDocument
+from .models import Notes, Label #, UpdatedUser
+
+
+# @admin.register(UpdatedUser)
+# class User(UserAdmin):
+#     class Meta:
+#         model = UpdatedUser
+
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['user', "note", 'title', 'image', 'is_archive', 'is_trashed', 'reminder']
+    list_filter = ['reminder']
+
+    class Meta:
+        model = Notes
+
+
+
+admin.site.register(Notes, NoteAdmin)
+admin.site.register(Label)
