419c98b09affb3f3614ce5c1ad62b11e9ca3eb3f
deepak mishra <dkm.5853@gmail.com>
2019-12-18 12:30:57 +0530

    [Deepak Mishra]Modified status.models.py

--- Restproject/statusApp/models.py
+++ Restproject/statusApp/models.py
@@ -1,10 +1,23 @@
+from django.conf import settings
 from django.db import models
 
 def upload_status_image(instance, filename):
     return "updates/{user}/{filename}".format(user= instance.user, filename=filename)
+class StatusQuerySet(models.QuerySet):
+    pass
+
+class StatusManager(models.Manager):
+    def get_queryset(self):
+        return StatusQuerySet(self.model, using=self._db)
 
 # Create your models here.
 class Status(models.Model):
-    user    = models.ForeignKey()
-    content = models.TextField(null=True,blank=True)
-    image   = models.ImageField(upload_to = upload_status_image, null=True, blank = True)
+    user      = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.PROTECT)
+    content   = models.TextField(null=True,blank=True)
+    image     = models.ImageField(upload_to = upload_status_image, null=True, blank = True)
+    updated   = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.content)[:50]
+          
