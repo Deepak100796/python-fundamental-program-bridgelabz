530e8e7d53a818681f82d8c23d2da8135f994720
deepak mishra <dkm.5853@gmail.com>
2019-12-28 14:34:23 +0530

    [Deepak Mishra]Modified note.views.py

--- fundoo/note/views.py
+++ fundoo/note/views.py
@@ -26,7 +26,7 @@ from django.utils.decorators import method_decorator
 from requests.auth import HTTPBasicAuth
 from requests_oauthlib import OAuth2Session
 from rest_framework import status
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView,mixins
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser, FileUploadParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -39,11 +39,16 @@ from .serialized import NotesSerializer
   # ArchiveSerializer
 # from lib.amazones3 import AmazoneS3
  # , label_coll_validator_post
-from lib.redis import red
+# from lib.redis import red
+# try:
+#     from fundoo import lib 
+# except ImportError as err:
+#     print(err)
+
 # import logging
 from .models import Notes, Label
 # from pymitter import EventEmitter
-
+import json
 # ee = EventEmitter()
 # s3 = AmazoneS3()
 
@@ -90,93 +95,126 @@ class NoteRetrive(generics.ListAPIView):
         
         serializer= NotesSerializer(queryset,many=True)
         return Response(serializer.data,status=200)
+from rest_framework.views import APIView
+class CreateNote(APIView):
+    def post(self, request, format=None):
+        serializer = NotesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = {"data":serializer.data, "status":status.HTTP_201_CREATED}
+            return Response(json.dumps(response))
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     # serializer_class = NotesSerializer
+#     # permission_classes = [IsAuthenticated]
+#     pass
+# class CreateNote(generics.ListAPIView,generics.CreateAPIView):
+    # queryset = Notes.objects.all()
+    # serializer_class = NotesSerializer
 
-class NoteRetriveAuth(generics.ListAPIView):
-    serializer_class = NotesSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
+   
 
-    def get_queryset(self,*args, **kwargs):
-        """
-        This view should return a list of all the purchases
-        for the currently authenticated user.
-        """
-        qs = Notes.objects.all()
-        qs=  qs.filter(User=self.request.user)
+    # def list(self, request):
+    #     return self.create(request)
+
+    #     # queryset = Notes.objects.all()
+
+    #     # Note the use of `get_queryset()` instead of `self.queryset`
+    #     # queryset = self.get_queryset()
+
+    #     # print(queryset)
+    #     # serializer = NotesSerializer(queryset, many=True)
+    #     # serializer.save()
+
+    # def show(self,request):
+    #     return self.get(request)
+
+    # def perform_save(self,requset):
+    #     return self.save(user=self.request.user)
 
 
     # parser_classes = (FormParser,FileUploadParser)
-    # @staticmethod
-    # def post(request):
-    #     """
-    #          Summary:
-    #          --------
-    #              New note will be create by the User.
-    #          Exception:
-    #          ----------
-    #              KeyError: object
-    #          Returns:
-    #          --------
-    #              response: SMD format of note create message or with error message
-    #     """
+    @staticmethod
+    def post(request):
+        """
+             Summary:
+             --------
+                 New note will be create by the User.
+             Exception:
+             ----------
+                 KeyError: object
+             Returns:
+             --------
+                 response: SMD format of note create message or with error message
+        """
 
-    #     user = request.user
-    #     try:
-    #         # data is taken from user
-    #         # pdb.set_trace()
-    #         data = request.data
-    #         if len(data) == 0:
-    #             raise KeyError
-    #         user = request.user
-    #         collaborator_list = []  # empty coll  list is formed where data is input is converted to id
-    #         try:
-    #             # for loop is used for the getting label input and coll input ids
-    #             data["label"] = [Label.objects.filter(user_id=user.id, name=name).values()[0]['id'] for name in
-    #                              data["label"]]
-    #         except KeyError:
-    #             # logger.debug('label was not added by the user %s', user)
-    #             pass
-    #         try:
-    #             collaborator = data['collaborators']
-    #             # for loop is used for the getting label input and coll input ids
-    #             for email in collaborator:
-    #                 email_id = User.objects.filter(email=email)
-    #                 user_id = email_id.values()[0]['id']
-    #                 collaborator_list.append(user_id)
-    #             data['collaborators'] = collaborator_list
-    #             print(data['collaborators'])
-    #         except KeyError:
-    #             # logger.debug('collaborator was not added by the user %s', user)
-    #             pass
-    #         serializer = NotesSerializer(data=data, partial=True)
-    #         if serializer.is_valid():
-    #             note_create = serializer.save(user_id=user.id)
-    #             response = {'success': True, 'message': "note created", 'data': []}
-    #             if serializer.data['is_archive']:
-    #                 red.hmset(str(user.id) + "is_archive",
-    #                           {note_create.id: str(json.dumps(serializer.data))})  # created note is cached in redis
-    #                 # logger.info("note is created for %s with note id as %s", user, note_create.id)
-    #                 return HttpResponse(json.dumps(response, indent=2), status=201)
-    #             else:
-    #                 if serializer.data['reminder']:
-    #                     red.hmset("reminder",
-    #                               {note_create.id: str(json.dumps({"email": user.email, "user": str(user),
-    #                                                                "note_id": note_create.id,
-    #                                                                "reminder": serializer.data["reminder"]}))})
-    #                 red.hmset(str(user.id) + "note",
-    #                           {note_create.id: str(json.dumps(serializer.data))})
+        user = request.user
+        # print(user)
+        try:
+            # data is taken from user
+            # pdb.set_trace()
+            data = request.data
+            # print(data)
+            if len(data) == 0:
+                raise KeyError
+            user = request.user
+            print(user)
 
-    #                 # logger.info("note is created for %s with note data as %s", user, note_create.__repr__())
-    #                 return HttpResponse(json.dumps(response, indent=2), status=201)
-    #         # logger.error(" %s for  %s", user, serializer.errors)
-    #         response = {'success': False, 'message': "note was not created", 'data': []}
-    #         return HttpResponse(json.dumps(response, indent=2), status=400)
-    #     except KeyError as e:
-    #         print(e)
-    #         # logger.error("got %s error for creating note as no data was provided for user %s", str(e), user)
-    #         response = {'success': False, 'message': "one of the field is empty ", 'data': []}
-    #         return Response(response, status=400)
-    #     except Exception as e:
-    #         print(e)
-    #         # logger.error("got %s error for creating note for user %s", str(e), user)
-    #         response = {'success': False, 'message': "something went wrong", 'data': []}
-    #         return Response(response, status=400)
+            collaborator_list = []  # empty coll  list is formed where data is input is converted to id
+            try:
+                # for loop is used for the getting label input and coll input ids
+                data["label"] = [Label.objects.filter(user_id=user.id, name=name).values()[0]['id'] for name in
+                                 data["label"]]
+                
+            except KeyError:
+                # logger.debug('label was not added by the user %s', user)
+                pass
+            try:
+                collaborator = data['collaborators']
+                
+                # for loop is used for the getting label input and coll input ids
+                for email in collaborator:
+                    email_id = User.objects.filter(email=email)
+                    user_id = email_id.values()[0]['id']
+                    collaborator_list.append(user_id)
+                data['collaborators'] = collaborator_list
+                print(data['collaborators'])
+            except KeyError:
+                # logger.debug('collaborator was not added by the user %s', user)
+                pass
+            serializer = NotesSerializer(data=data, partial=True)
+            
+            if serializer.is_valid():
+                # print(serializer)
+                note_create = serializer.save(user_id=user.id)
+                response = {'success': True, 'message': "note created", 'data': []}
+                if serializer.data['is_archive']:
+                    lib.red.hmset(str(user.id) + "is_archive",
+                              {note_create.id: str(json.dumps(serializer.data))}) 
+        #                        # created note is cached in redis
+        #             # logger.info("note is created for %s with note id as %s", user, note_create.id)
+                    return HttpResponse(json.dumps(response, indent=2), status=201)
+        #         else:
+                    if serializer.data['reminder']:
+                        lib.red.hmset("reminder",
+                                  {note_create.id: str(json.dumps({"email": user.email, "user": str(user),
+                                                                   "note_id": note_create.id,
+                                                                   "reminder": serializer.data["reminder"]}))})
+                    lib.red.hmset(str(user.id) + "note",
+                              {note_create.id: str(json.dumps(serializer.data))})
+
+        #             # logger.info("note is created for %s with note data as %s", user, note_create.__repr__())
+        #             return HttpResponse(json.dumps(response, indent=2), status=201)
+        #     # logger.error(" %s for  %s", user, serializer.errors)
+        #     response = {'success': False, 'message': "note was not created", 'data': []}
+        #     return HttpResponse(json.dumps(response, indent=2), status=400)
+        except KeyError as e:
+            print(e)
+            # logger.error("got %s error for creating note as no data was provided for user %s", str(e), user)
+            response = {'success': False, 'message': "one of the field is empty ", 'data': []}
+            return Response(response, status=400)
+        except Exception as e:
+            print(e)
+            # logger.error("got %s error for creating note for user %s", str(e), user)
+            response = {'success': False, 'message': "something went wrong", 'data': []}
+            return Response(response, status=400)
